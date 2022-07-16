13ceeacf93ee2cf4be0d6b7479bd9b5389e7fede
hujy009 <hujy009@gmail.com>
2022-07-15 22:59:17 -0400

    stage 2 of the Tic Tac Toe game

--- board.py
+++ board.py
@@ -8,4 +8,55 @@ def print_board():
         print(e)
 
 
-print_board()
+
+def isValid(row, col):
+    if row < 0 or row > 2 or col < 0 or col > 2:
+        print(f"the row or col is out of the range. Pleae select in [0, 1, 2]")
+        return False
+    if board[row][col] != '-':
+        print(f"---board[{row}][{col}] has been selected. Please reselect somewhere else on the board---")
+        return False
+    return True
+
+
+def mark_board(player, row, col):
+    if player ==  1:
+        board[row][col] = 'X'
+    if player == 2:
+        board[row][col] = 'O'
+    
+    for e in board:
+        if '-' in e:
+            return -1
+    return player 
+
+
+  
+if __name__ == "__main__":
+    num = 0      
+player = 1
+while True:
+    if num % 2 == 0:
+        player = 1
+    else:
+        player = 2
+    print_board()
+    
+    print(f"\nPlayer {player}, place a mark on the board")
+    row = int(input("Enter row number between 0 (inclusive), 2(inclusive)"))
+    col = int(input("Enter col number between 0 (inclusive), 2(inclusive)"))
+
+    if not isValid(row, col):
+        print("---Invalid input choice. Please mark again!---")
+        continue
+    
+    whoWin = mark_board(player, row, col)
+    #print_board()
+
+    if whoWin == 1:
+        print(" Player 1 wins! Game Over!")
+        break
+    elif whoWin == 2:
+        print(" Player 2 wins! Game Over!")
+        break
+    num = num + 1
